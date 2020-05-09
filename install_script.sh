301f8bb86f734c049ad1545b87b054148c9bffbf
Sanket Bodele <sanketssj5@gmail.com>
2020-05-09 18:40:13 +0530

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,2 +1,7 @@
 #!/bin/bash  
-echo "This is a shell script"  
+sudo apt -y install vim
+echo -e "set number\nsyntax on\nset laststatus=2" >> ~/.vimrc
+sudo apt -y install tmux
+echo "setw -g mouse on" >> ~/.tmux.conf
+
+source ~/.bashrc
